98aafdfe5b47dba93099e6c1a28acbaf17ec05b9
ClixStudios <u1555999@unimail.hud.ac.uk>
2020-01-20 13:25:37 +0000

    Initial v0.1.0 commit (server)
    Added forwarding routes (between client and server namespace) for:
    PlayerJoined
    VirtualButtonUpdate
    VirtualControllerUpdate
    Disconnect
    GetPlayers
    NewHUDLayout
    CustomPlayerCommand

---
+++ index.js
@@ -0,0 +1,78 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+const serverNamespace = io.of('/server');
+const clientNamespace = io.of('/client');
+
+let lobbyPlayers = [];
+
+app.get('/', function (req, res) {
+      console.log('dir: /')
+      res.sendFile(__dirname + '/html/index.html');
+});
+
+clientNamespace.on('connection', function (socket) {
+
+      console.log('Client connected');
+
+      lobbyPlayers.push({ clientId: socket.id })
+
+      // Tell server about new player
+      serverNamespace.emit('PlayerJoined', {clientId:socket.id});
+
+      // Tell server about new Virtual Controller Input
+      socket.on('VirtualControllerUpdate', function (data) {
+            data.clientId = socket.id;
+            serverNamespace.emit('VirtualControllerUpdate', data);
+      });
+
+      // Tell server about new Virtual Controller Input
+      socket.on('VirtualButtonUpdate', function (data) {
+            data.clientId = socket.id;
+            serverNamespace.emit('VirtualButtonUpdate', data);
+      });
+
+      socket.on('disconnect', function () {
+            console.log('Client disconnected');
+            
+            // Remove player from array
+            lobbyPlayers = lobbyPlayers.filter((value, index, array)=>{
+                  return !(value.clientId == socket.id);
+            })
+      });
+
+});
+
+serverNamespace.on('connection', function (socket) {
+      console.log('Server connected');
+
+      socket.on('disconnect', function () {
+            clientNamespace.emit('ServerDisconnected');
+            console.log('Server disconnected');
+      });
+
+      socket.on('GetPlayers', (data, cb) => {
+            cb(lobbyPlayers);
+      });
+
+      socket.on('NewHUDLayout', function (data) {
+            console.log(data);
+            if (data.HUDElements) {
+                  // TODO: Data validation (not needed for the prototype)
+                  clientNamespace.emit('NewHUDLayout', data);
+            }
+      });
+
+      socket.on('CustomPlayerCommand', function (data) {
+            console.log(data);
+            // TODO: Data validation (not needed for the prototype)
+            for (const playerClientId of data.recipientPlayers) {
+                  clientNamespace.to(`${playerClientId}`).emit(data.eventName, data.data);
+            }
+      });
+});
+
+http.listen(80, function () {
+      console.log('listening on *:80');
+}); 
