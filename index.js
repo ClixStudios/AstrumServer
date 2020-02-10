9ef8e2c423baacf97936f6d9ffd621f7d01c7a27
ClixStudios <u1555999@unimail.hud.ac.uk>
2020-02-10 13:36:23 +0000

    Starting to add features (read desc)
    Starting to add image send features and clientId persistance

--- index.js
+++ index.js
@@ -8,18 +8,23 @@ const clientNamespace = io.of('/client');
 let lobbyPlayers = [];
 
 app.get('/', function (req, res) {
-      console.log('dir: /')
+      console.log('dir: /');
       res.sendFile(__dirname + '/html/index.html');
 });
+
+app.get('/image', function (req, res){
+      console.log('Sending Image');
+      res.sendFile(__dirname + '/image.png');
+})
 
 clientNamespace.on('connection', function (socket) {
 
       console.log('Client connected');
 
-      lobbyPlayers.push({ clientId: socket.id })
+      lobbyPlayers.push({ clientId: socket.id });
 
       // Tell server about new player
-      serverNamespace.emit('PlayerJoined', {clientId:socket.id});
+      serverNamespace.emit('PlayerJoined', { clientId: socket.id });
 
       // Tell server about new Virtual Controller Input
       socket.on('VirtualControllerUpdate', function (data) {
@@ -31,15 +36,16 @@ clientNamespace.on('connection', function (socket) {
       socket.on('VirtualButtonUpdate', function (data) {
             data.clientId = socket.id;
             serverNamespace.emit('VirtualButtonUpdate', data);
+            console.log(data);
       });
 
       socket.on('disconnect', function () {
             console.log('Client disconnected');
-            
+
             // Remove player from array
-            lobbyPlayers = lobbyPlayers.filter((value, index, array)=>{
+            lobbyPlayers = lobbyPlayers.filter((value, index, array) => {
                   return !(value.clientId == socket.id);
-            })
+            });
       });
 
 });
@@ -62,6 +68,18 @@ serverNamespace.on('connection', function (socket) {
                   // TODO: Data validation (not needed for the prototype)
                   clientNamespace.emit('NewHUDLayout', data);
             }
+      });
+
+      socket.on('UpdateHUDElement', (data) => {
+            clientNamespace.emit('UpdateHUDElement', data);
+      });
+
+      socket.on('Vibrate', (data) => {
+            clientNamespace.emit('Vibrate', data);
+      });
+
+      socket.on('Speak', (data) => {
+            clientNamespace.emit('Speak', data);
       });
 
       socket.on('CustomPlayerCommand', function (data) {
